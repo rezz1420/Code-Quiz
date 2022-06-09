57132b9f3318cc8d032b3231139ffff16991912e
Rez <rezowanmalek@gmail.com>
2022-06-09 06:45:27 +0100

    CSS corrections has been made and Js implemented

--- game.js
+++ game.js
@@ -1,1 +1,112 @@
-const question = document.querySelector{''}
+const question = document.querySelector("#question");
+const choices = Array.from(document.querySelectorAll(".choice-text"));
+const progressText = document.querySelector("#progressText");
+const scoreText = document.querySelector("#score");
+const progressBarFull = document.querySelector("#progressBarFull");
+
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+let questions = [
+  {
+    question: "What is 2 + 6?",
+    choice1: "2",
+    choice2: "8",
+    choice3: "21",
+    choice4: "9",
+    answer: 2,
+  },
+  {
+    question: "Which is the tallest building on Earth?",
+    choice1: "Burj Khalifa",
+    choice2: "Empire State Building",
+    choice3: "Shanghai tower",
+    choice4: "Makkah Royal CLock Tower",
+    answer: 1,
+  },
+  {
+    question: "What is the population of Earth currently?",
+    choice1: "9 Billion",
+    choice2: "8.3 Billion",
+    choice3: "7.6 Billion",
+    choice4: "7.23 Billion",
+    answer: 3,
+  },
+  {
+    question: "Who deciphered the Enigma Code?",
+    choice1: "Albert Einstein",
+    choice2: "Alan Turing",
+    choice3: "Stephen Hawking",
+    choice4: "Srinivasa Ramanujan",
+    answer: 2,
+  },
+];
+
+const SCORE_POINTS = 100;
+const MAX_QUESTIONS = 4;
+
+startGame = () => {
+  questionCounter = 0;
+  score = 0;
+  availableQuestions = [...questions];
+  getNewQuestion();
+};
+
+getNewQuestion = () => {
+  if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+    localStorage.setItem("mostRecentScore", score);
+
+    return window.location.assign("/end.html");
+  }
+
+  questionCounter++;
+  progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`;
+  progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+
+  const questionsIndex = Math.floor(Math.random() * availableQuestions.length);
+  currentQuestion = availableQuestions[questionsIndex];
+  question.innerText = currentQuestion.question;
+
+  choices.forEach((choice) => {
+    const number = choice.dataset["number"];
+    choice.innerText = currentQuestion["choice" + number];
+  });
+
+  availableQuestions.splice(questionsIndex, 1);
+
+  acceptingAnswers = true;
+};
+
+choices.forEach((choice) => {
+  choice.addEventListener("click", (e) => {
+    if (!acceptingAnswers) return;
+
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset["number"];
+
+    let classToApply =
+      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+
+    if (classToApply === "correct") {
+      incrementScore(SCORE_POINTS);
+    }
+
+    selectedChoice.parentElement.classList.add(classToApply);
+
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply);
+      getNewQuestion();
+    }, 1000);
+  });
+});
+
+incrementScore = (num) => {
+  score += num;
+  scoreText.innerText = score;
+};
+
+startGame();
